f896685d8a3dc1d0a84831e7791a93bd89eadd61
wenfangdi <158525259+wenfangdi@users.noreply.github.com>
2025-10-17 16:05:35 -0700

    Update 10-16-2025.sql

--- 10-16-2025.sql
+++ 10-16-2025.sql
@@ -6,3 +6,24 @@ and step_parameter = 'Longer Dimension'
 and safe_cast(data as float64) > 40
 and timestamp > current_timestamp() - interval 30 day
 group by 1,2
+
+-- some growth with improper operation
+select id_run_growth,growing_location_name, count(id_block), string_agg(cast(id_block as string)) from
+(select case when bgr.run_duration >= 690 and bgr.hardware_platform like 'Gen7.5%' then 'Gem' 
+    when bgr.run_duration >= 770 and bgr.hardware_platform like 'Gen8%' then 'Gem'
+    when bgr.run_duration >= 270 and bgr.run_duration < 350 then 'Plate' end as run_type,
+    date(bgr.end_time, 'America/Los_Angeles') as end_date, bgbd.id_block, bgr.run_duration,
+    bgbd.new_thickness, bgbd.pre_thickness, bgbd.abs_clean_area_rect_210,
+    (bgbd.new_thickness - bgbd.pre_thickness) * bgbd.abs_clean_area_rect_210 as final_clean_vol_cm3,
+    bgr.id_run_growth, bgr.growing_location_name
+    from `df-data-and-ai`.trilliback.bq_growth_block_data bgbd
+    inner join `df-data-and-ai`.trilliback.bq_growth_runs bgr 
+    on bgbd.id_run_growth = bgr.id_run_growth 
+    where date(bgr.end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 30 day 
+    and date(bgr.end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
+)
+where run_type is not null
+and final_clean_vol_cm3 is null
+and end_date < '2025-10-16'
+and growing_location_name = 'Diamond Foundry WA'
+group by 1,2
