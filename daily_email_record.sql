a1a0999edd170d3ae5411264b2b250767704ed12
wenfangdi <158525259+wenfangdi@users.noreply.github.com>
2025-10-27 09:21:54 -0700

    Update daily_email_record.sql
    format it to look better

--- daily_email_record.sql
+++ daily_email_record.sql
@@ -1,74 +1,67 @@
 -- before new flow implementation 
-    with throughput_raw as 
+       with 
+    step_table as (
+      SELECT '301005' as step_name, '2-Ingot In' as segment
+      UNION ALL
+      SELECT '302020', '3-Cored'
+      UNION ALL 
+      SELECT '304020', '4-TDLed'
+      UNION ALL
+      SELECT '304060', '5-Splitted'
+      UNION ALL
+      SELECT '306010', '6-Trimmed'
+      UNION ALL
+      SELECT '306006', '7-Shaved'
+    ),
+    throughput_raw as 
     (select   
     '1x1' as master_product,
     end_time,
     -- mh.product_name_last,
     timestamp_trunc(DATETIME(TIMESTAMP(end_time), "America/Los_Angeles"), DAY) as end_date_PST, 
-    case 
-    when mh.step_name = '301005' then '2-Ingot In'
-    when mh.step_name = '302020' then '3-Cored'
-    when mh.step_name = '304020' then '4-TDLed'
-    when mh.step_name = '304060' then '5-Splitted'
-    when mh.step_name = '306010' then '6-Trimmed'
-    when mh.step_name = '306006' then '7-Shaved'
-    end
-    as segment,
+    step_table.segment,
     flow_name_in as flow_name,
     1 as out,
     bd.thickness,
     mh.id_block
     from
     (
-    select start_time, end_time, id_block, material_type_last, product_name_last, 
-    REGEXP_REPLACE(
-    REGEXP_REPLACE(step_name, r'^403', '303'),
-    r'^404', '304'
-    ) AS step_name,
-    REGEXP_REPLACE(
-    REGEXP_REPLACE(step_name_next, r'^403', '303'),
-    r'^404', '304'
-    ) AS step_name_next,
-    step_description_in, product_id_next,material_name_in,manufacturer_lot_number_in, flow_name_in 
-    from
-    df-mes.mes_warehouse.block_step_tracker
+        select start_time, end_time, id_block, material_type_last, product_name_last, 
+        REGEXP_REPLACE(
+        REGEXP_REPLACE(step_name, r'^403', '303'),
+        r'^404', '304'
+        ) AS step_name,
+        REGEXP_REPLACE(
+        REGEXP_REPLACE(step_name_next, r'^403', '303'),
+        r'^404', '304'
+        ) AS step_name_next,
+        step_description_in, product_id_next,material_name_in,manufacturer_lot_number_in, flow_name_in 
+        from
+        df-mes.mes_warehouse.block_step_tracker
     ) mh
+    JOIN step_table on step_table.step_name = mh.step_name
     left join df-max.raw_dfdb.block_dimensions bd
     on mh.id_block = bd.id_block
-    left join 
-    (select master_product, sub_product_name, sub_product_id as product_id from df-mes.mes_warehouse.master_products 
-    where product_group = 'Product Reporting Layer') mp 
-    on mh.product_id_next = mp.product_id
-    join df-mes.mes_warehouse.flow_segments_new fs 
-    on fs.segment_id = substring(mh.step_name,1,3)
     where mh.flow_name_in = 'TDL-Seed_V0L'
     and mh.end_time BETWEEN timestamp_trunc(current_timestamp() - interval 14+1 day,DAY, "America/Los_Angeles") AND timestamp_trunc(current_timestamp(),DAY, "America/Los_Angeles")
-    and mh.step_name in (
-    '301005', -- Ingot In
-    '302020', -- cored
-    '304020', -- TDLed use presumed plate count
-    '304060', -- splitted
-    '306010', -- trimmed
-    '306006' -- shaved 
-    )
     and end_time is not null
     ),
     throughput as (
-    select *,
-    case 
-    when segment in ('2-Ingot In','3-Cored', '4-TDLed') then floor(thickness/460)*out
-    else out
-    end as plate_count,
-    master_product as plate_size, 
-    from throughput_raw
-    where end_time BETWEEN timestamp_trunc(current_timestamp() - interval 14+1 day,DAY, "America/Los_Angeles") AND current_timestamp()
+      select *,
+      case 
+      when segment in ('2-Ingot In','3-Cored', '4-TDLed') then floor(thickness/460)*out
+      else out
+      end as plate_count,
+      master_product as plate_size, 
+      from throughput_raw
+      where end_time BETWEEN timestamp_trunc(current_timestamp() - interval 14+1 day,DAY, "America/Los_Angeles") AND current_timestamp()
     ),
     segments as 
     (select segment from unnest(['2-Ingot In','3-Cored', '4-TDLed', '5-Splitted','6-Trimmed','7-Shaved']) segment),
     periods as 
     (select period from unnest(['14d', '5d', '1d']) period),
     master_products as 
-    (select master_product from unnest([ '1x1', '2x2', 'D200']) master_product),
+    (select master_product from unnest([ '1x1']) master_product),
     seg_col as (
     select segment, period, master_product as plate_size 
     from segments
@@ -78,16 +71,16 @@
     on true
     ),
     grown as (
-    select 
-    seed_size_type as seed_type,
-    b.id_block,
-    b.end_time,
-    greatest(IFNULL(floor(new_thickness / 460 - 2),0), 1) as plate_count
-    from `df-data-and-ai.trilliback.bq_growth_block_data` b
-    join `df-data-and-ai.trilliback.bq_growth_runs_about_block` r using(id_run_growth) 
-    where date(b.end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 14 day 
-    and date(b.end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
-    and r.seed_size_type != 'Others'
+      select 
+      seed_size_type as seed_type,
+      b.id_block,
+      b.end_time,
+      greatest(IFNULL(floor(new_thickness / 460 - 2),0), 1) as plate_count
+      from `df-data-and-ai.trilliback.bq_growth_block_data` b
+      join `df-data-and-ai.trilliback.bq_growth_runs_about_block` r using(id_run_growth) 
+      where date(b.end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 14 day 
+      and date(b.end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
+      and r.seed_size_type != 'Others'
     ),    
     seg_col_product_output as (
     select seg_col.segment, seg_col.period as col, seg_col.plate_size,
@@ -111,46 +104,52 @@
     on d.segment = seg_col.segment 
     and d.plate_size = seg_col.plate_size 
     and d.period = seg_col.period
+
+
+    -- UNION ALL
+    -- SELECT '8-Stock/30' as segment, '5d' as period, -- bar location holder no real meaning
+    -- case when master_product = '20x20' THEN 'Fillers'
+    -- when master_product = '33x26' THEN '1x1'
+    -- else 'Fillers' 
+    -- end as plate_size, 
+    -- sum(plate_count) / 30 as output 
+    -- from df-mes.mes_warehouse.WIP_Cache_plate 
+    -- WHERE master_product in ('33x26')
+    -- and flow_name = 'TDL-Seed_V0L'
+    -- group by 1, 2, 3   
+
     UNION ALL
-    SELECT '8-Stock/30' as segment, '5d' as period, -- bar location holder no real meaning
-    case when master_product = '20x20' THEN 'Fillers'
-    when master_product = '33x26' THEN '1x1'
-    else 'Fillers' 
-    end as plate_size, 
-    sum(plate_count) / 30 as output 
-    from df-mes.mes_warehouse.WIP_Cache_plate 
-    WHERE master_product in ('33x26')
-    and flow_name = 'TDL-Seed_V0L'
-    group by 1, 2, 3   
-    UNION ALL
+
     select '1-Grown' as segment, periods.period, master_product, output
     from periods 
     join master_products
     on true
-    left join    
-    (select '14d' as period,
-    seed_type,
-    sum(plate_count) / 14 as output
-    from grown
-    where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 14 day 
-    and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
-    group by 1, 2
+    left join  
+    (
+      select '14d' as period,
+      seed_type,
+      sum(plate_count) / 14 as output
+      from grown
+      where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 14 day 
+      and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
+      group by 1, 2
     UNION ALL
-    select '5d' as period,
-    seed_type,
-    sum(plate_count) / 5 as output
-    from grown
-    where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 5 day 
-    and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
-    group by 1, 2
+      select '5d' as period,
+      seed_type,
+      sum(plate_count) / 5 as output
+      from grown
+      where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 5 day 
+      and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
+      group by 1, 2
     UNION ALL
-    select '1d' as period,
-    seed_type,
-    sum(plate_count)  as output
-    from grown
-    where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 1 day 
-    and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
-    group by 1, 2) yield
+      select '1d' as period,
+      seed_type,
+      sum(plate_count)  as output
+      from grown
+      where date(end_time, 'America/Los_Angeles') >= current_date('America/Los_Angeles') - interval 1 day 
+      and date(end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
+      group by 1, 2
+      ) yield
     on periods.period = yield.period
     and master_products.master_product = yield.seed_type    
     )
