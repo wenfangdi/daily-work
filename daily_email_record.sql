67bced2bedcf7002a63a89f0cc2479cd376fb8a0
wenfangdi <158525259+wenfangdi@users.noreply.github.com>
2025-10-28 10:25:45 -0700

    Update daily_email_record.sql

--- daily_email_record.sql
+++ daily_email_record.sql
@@ -1,62 +1,49 @@
--- before new flow implementation 
+-- under new flow
        with 
     step_table as (
-      SELECT '301005' as step_name, '2-Ingot In' as segment
-      UNION ALL
-      SELECT '302020', '3-Cored'
+      SELECT '702020' as step_name, '3-Cored' as segment
       UNION ALL 
-      SELECT '304020', '4-TDLed'
+      SELECT '704020', '4-TDLed'
       UNION ALL
-      SELECT '304060', '5-Splitted'
+      SELECT '704060', '5-Splitted' -- breeding
       UNION ALL
-      SELECT '306010', '6-Trimmed'
+      SELECT '704110', '6-Trimmed'
       UNION ALL
-      SELECT '306006', '7-Shaved'
+      SELECT '704105', '7-Shaved'
     ),
-    throughput_raw as 
+throughput_raw as 
     (select   
     '1x1' as master_product,
     end_time,
-    -- mh.product_name_last,
     timestamp_trunc(DATETIME(TIMESTAMP(end_time), "America/Los_Angeles"), DAY) as end_date_PST, 
-    step_table.segment,
+    IFNULL(step_table.segment, '2-Ingot In') as segment,
     flow_name_in as flow_name,
     1 as out,
     bd.thickness,
     mh.id_block
     from
-    (
-        select start_time, end_time, id_block, material_type_last, product_name_last, 
-        REGEXP_REPLACE(
-        REGEXP_REPLACE(step_name, r'^403', '303'),
-        r'^404', '304'
-        ) AS step_name,
-        REGEXP_REPLACE(
-        REGEXP_REPLACE(step_name_next, r'^403', '303'),
-        r'^404', '304'
-        ) AS step_name_next,
-        step_description_in, product_id_next,material_name_in,manufacturer_lot_number_in, flow_name_in 
-        from
-        df-mes.mes_warehouse.block_step_tracker
-    ) mh
-    JOIN step_table on step_table.step_name = mh.step_name
+        `df-mes.mes_warehouse.block_step_tracker` mh
+    left join step_table on mh.step_name = step_table.step_name
     left join df-max.raw_dfdb.block_dimensions bd
     on mh.id_block = bd.id_block
-    where mh.flow_name_in = 'TDL-Seed_V0L'
+    where mh.flow_name_in in ('MPF I&D','MPF')
     and mh.end_time BETWEEN timestamp_trunc(current_timestamp() - interval 14+1 day,DAY, "America/Los_Angeles") AND timestamp_trunc(current_timestamp(),DAY, "America/Los_Angeles")
+    and ( (mh.step_name_next = '702020') or 
+          (mh.step_name in (select step_name from step_table))
+        )
     and end_time is not null
     ),
-    throughput as (
+throughput as (
       select *,
       case 
-      when segment in ('2-Ingot In','3-Cored', '4-TDLed') then floor(thickness/460)*out
+      when segment in ('2-Ingot In','3-Cored', '4-TDLed') then floor(IF(thickness != 0, thickness, 460)/460)*out
       else out
       end as plate_count,
       master_product as plate_size, 
       from throughput_raw
       where end_time BETWEEN timestamp_trunc(current_timestamp() - interval 14+1 day,DAY, "America/Los_Angeles") AND current_timestamp()
     ),
-    segments as 
+segments as 
     (select segment from unnest(['2-Ingot In','3-Cored', '4-TDLed', '5-Splitted','6-Trimmed','7-Shaved']) segment),
     periods as 
     (select period from unnest(['14d', '5d', '1d']) period),
@@ -70,7 +57,7 @@
     join master_products
     on true
     ),
-    grown as (
+grown as (
       select 
       seed_size_type as seed_type,
       b.id_block,
@@ -82,7 +69,7 @@
       and date(b.end_time, 'America/Los_Angeles') < current_date('America/Los_Angeles')
       and r.seed_size_type != 'Others'
     ),    
-    seg_col_product_output as (
+seg_col_product_output as (
     select seg_col.segment, seg_col.period as col, seg_col.plate_size,
     d.output as output
     from seg_col
@@ -90,12 +77,12 @@
     (select segment, '14d' as period, plate_size, sum(plate_count) / 14 as output 
     from throughput
     group by segment, plate_size
-    UNION ALL
+  UNION ALL
     select segment, '5d' as period, plate_size, sum(plate_count) / 5 as output 
     from throughput
     where end_date_PST BETWEEN timestamp_trunc(datetime(current_timestamp() - interval 5+1 day, "America/Los_Angeles"), DAY) AND current_datetime()
     group by segment, plate_size
-    UNION ALL
+  UNION ALL
     select segment, '1d' as period, plate_size, sum(plate_count) /1 as output 
     from throughput
     where end_date_PST BETWEEN timestamp_trunc(datetime(current_timestamp() - interval 1 day, "America/Los_Angeles"), DAY) AND current_datetime()
@@ -104,19 +91,6 @@
     on d.segment = seg_col.segment 
     and d.plate_size = seg_col.plate_size 
     and d.period = seg_col.period
-
-
-    -- UNION ALL
-    -- SELECT '8-Stock/30' as segment, '5d' as period, -- bar location holder no real meaning
-    -- case when master_product = '20x20' THEN 'Fillers'
-    -- when master_product = '33x26' THEN '1x1'
-    -- else 'Fillers' 
-    -- end as plate_size, 
-    -- sum(plate_count) / 30 as output 
-    -- from df-mes.mes_warehouse.WIP_Cache_plate 
-    -- WHERE master_product in ('33x26')
-    -- and flow_name = 'TDL-Seed_V0L'
-    -- group by 1, 2, 3   
 
     UNION ALL
 
@@ -153,28 +127,28 @@
     on periods.period = yield.period
     and master_products.master_product = yield.seed_type    
     )
-    select 
-    case 
-    when segment = '1-Grown' then 'Ingots Grown'
-    when segment = '2-Ingot In' then 'Ingots In@BE'
-    when segment = '3-Cored' then 'Core'
-    when segment = '4-TDLed' then 'TDL'
-    when segment = '5-Splitted' then 'Split'
-    when segment = '6-Trimmed' then 'Trim'
-    when segment = '7-Shaved' then 'Shave'
-    when segment = '8-Stock/30' then 'Stock/30'
-    end as step, 
-    safe_cast(left(segment, 1) as int64) - 1 as step_number,
-    col as period,
-    coalesce(sum(
-        case when plate_size = '1x1' then output
-        when plate_size = '2x2' then output * 4
-        when plate_size = 'D200' then output * 36
-        else output end), 0.01) as yield,
-    plate_size,
-    from seg_col_product_output
-    group by 1, 2, 3, 5
-    order by 2, 3, 5;
+select 
+case 
+when segment = '1-Grown' then 'Ingots Grown'
+when segment = '2-Ingot In' then 'Ingots In@BE'
+when segment = '3-Cored' then 'Core'
+when segment = '4-TDLed' then 'TDL'
+when segment = '5-Splitted' then 'Split'
+when segment = '6-Trimmed' then 'Trim'
+when segment = '7-Shaved' then 'Shave'
+when segment = '8-Stock/30' then 'Stock/30'
+end as step, 
+safe_cast(left(segment, 1) as int64) - 1 as step_number,
+col as period,
+coalesce(sum(
+    case when plate_size = '1x1' then output
+    when plate_size = '2x2' then output * 4
+    when plate_size = 'D200' then output * 36
+    else output end), 0.01) as yield,
+plate_size,
+from seg_col_product_output
+group by 1, 2, 3, 5
+order by 2, 3, 5;
 
 -- for cycle time
 
