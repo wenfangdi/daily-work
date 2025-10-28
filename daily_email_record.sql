fd8d08d780c600b0435571e93bdaf53423a3e056
wenfangdi <158525259+wenfangdi@users.noreply.github.com>
2025-10-28 12:10:37 -0700

    Update daily_email_record.sql
     change the cycle time logic

--- daily_email_record.sql
+++ daily_email_record.sql
@@ -152,38 +152,41 @@ order by 2, 3, 5;
 
 -- for cycle time
 
-   with blocks_to_search as (
+   with 
+   fs as ( -- flow segments
+    SELECT step_name, segment_name
+    FROM UNNEST([
+      STRUCT('702020' AS step_name, 'Core' AS segment_name),
+      ('702030','Core'),
+      ('702035','Core'),
+      ('703015','RIM'),
+      ('703020','RIM'),
+      ('704020','TDL')
+    ])
+   ),
+   sc as ( --segment change
+    SELECT step_name,step_name_next, segment_name
+    FROM UNNEST([
+      STRUCT('702035' AS step_name,'703015' as step_name_next, 'Core' AS segment_name),
+      ('703020','704020', 'RIM')
+    ])
+   ),
+   blocks_to_search as (
     select
-    fp.id_block,
-    blocks.parent_id_block,
-    fp.timestamp as WS_ship_out_time,
-    fps.size_family,
-    FORMAT_TIMESTAMP('%Y-%U', DATETIME(fp.timestamp, 'America/Los_Angeles') + INTERVAL 1 WEEK) AS WS_ship_out_week,
-    max(fps.surface_finish) as PIM_surface_finish,
-    max(fps.output_category) as PIM_output_category
-    from `df-max.raw_dfdb.final_plates` fp
-    join `df-max.raw_dfdb.final_plate_shipments` fps using(id_final_plate_shipment)
-    join `df-max.raw_dfdb.blocks` blocks on fp.id_block = blocks.id_block
-    where fp.timestamp > timestamp_trunc(current_timestamp() - interval 90 day, day, 'America/Los_Angeles')
-    and fp.timestamp <  timestamp_trunc(current_timestamp(), day, 'America/Los_Angeles')
-    group by 1,2,3,4
+    id_block,sc.segment_name,
+    max(end_time) as segment_finish_time
+    from `df-mes.mes_warehouse.block_step_tracker`
+    join sc using(step_name, step_name_next) 
+    where end_time > timestamp_trunc(current_timestamp() - interval 90 day, day, 'America/Los_Angeles')
+    and end_time <  timestamp_trunc(current_timestamp(), day, 'America/Los_Angeles')
+    group by 1,2
     ),
-    record_raw as (
+  record_raw as (
     select
-    mh.id_block as processed_block,
-    blocks_to_search.id_block as child_plate,
-    blocks_to_search.WS_ship_out_time,
-    blocks_to_search.parent_id_block,
-    case 
-    when fs.segment_name = 'Shave/Trim' then
-      case when mh.step_name in ('306010') then 'Trim'
-      when mh.step_name in ('306006') then 'Shave'
-      else 'Shave/Trim Buffer and Char' end
-    when fs.segment_name = 'TDL' then
-      case when mh.step_name in ('304020') then 'TDL'
-      when mh.step_name in ('304030','304040','304050','304060','304062') then 'Split'
-      else 'TDL / Split' end
-    else fs.segment_name end as segment_name,
+    mh.id_block,
+    step_name, step_name_next,end_time,
+    fs.segment_name as segment_name,
+    blocks_to_search.segment_finish_time,
     manufacturer_lot_number_in as batch,
     cycle_time/3600 as cycle_hour,
     product_name_in as product_name,
@@ -191,49 +194,45 @@ order by 2, 3, 5;
     area_name_in as area_name,
     Concat(step_name,'-',IFNULL(step_description_in, '')) as step
     from `df-mes.mes_warehouse.block_step_tracker` mh
-    JOIN blocks_to_search on (mh.id_block = blocks_to_search.id_block or mh.id_block = blocks_to_search.parent_id_block)
-    join `df-mes.mes_warehouse.flow_segments_new` fs on fs.segment_id = substring(mh.step_name,1,3)
+    JOIN blocks_to_search on (mh.id_block = blocks_to_search.id_block)
+    join fs using(step_name, segment_name)
     where end_time > current_timestamp() - interval 365 day
     and step_name_next is not null
     and cycle_time is not null
-    and material_type_in != 'Software Test'
+    -- and material_type_in != 'Software Test'
     -- and segment_name in ('Coring', 'TDL','Shave/Trim')
     ),
     cycle_time_per_block as (
-    select child_plate, case when segment_name in ('Coring') then 'Core'
-    when segment_name in ('TDL') then 'TDL'
-    when segment_name in ('Split') then 'Split'
-    when segment_name in ('TDL / Split') then 'TDL / Split'
-    when segment_name in ('Trim') then 'Trim'
-    when segment_name in ('Shave') then 'Shave'
-    when segment_name in ('Shave/Trim Buffer and Char') then 'Trim / Shave'
-    end as segment_name, WS_ship_out_time, sum(cycle_hour) as cycle_hour, 
+    select id_block, segment_name, segment_finish_time, sum(cycle_hour) as cycle_hour, 
     from record_raw
     group by 1, 2, 3
     ),
-    segments as 
+  segments as 
     (select segment_name, segment_rank from unnest(['Core', 'TDL', 'Split', 'Trim', 'Shave', 'TDL / Split', 'Trim / Shave']) segment_name with offset as segment_rank),
-    periods as 
+  periods as 
     (select period, period_rank from unnest(['90d', '30d', '7d']) period with offset as period_rank),
-    seg_p as 
+  seg_p as 
     (select * from segments join periods on true)
-    select distinct seg_p.*, coalesce(t.cycle_hour_median, 0.01) as cycle_hour_median
-    from seg_p 
-    left join 
-    (select segment_name, '90d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
-    PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
-    from cycle_time_per_block
-    where WS_ship_out_time > timestamp_trunc(current_timestamp() - interval 90 day, day, 'America/Los_Angeles')
-    UNION ALL
-    select segment_name, '30d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
-    PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
-    from cycle_time_per_block
-    where WS_ship_out_time > timestamp_trunc(current_timestamp() - interval 30 day, day, 'America/Los_Angeles')
-    UNION ALL
-    select segment_name, '7d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
-    PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
-    from cycle_time_per_block
-    where WS_ship_out_time > timestamp_trunc(current_timestamp() - interval 7 day, day, 'America/Los_Angeles')) t
-    on seg_p.segment_name = t.segment_name
-    and seg_p.period = t.time_interval
-    order by segment_rank, period_rank;
+
+
+--------------------------------------------------
+  select distinct seg_p.*, coalesce(t.cycle_hour_median, 0.01) as cycle_hour_median
+  from seg_p 
+  left join 
+  (select segment_name, '90d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
+  PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
+  from cycle_time_per_block
+  where segment_finish_time > timestamp_trunc(current_timestamp() - interval 90 day, day, 'America/Los_Angeles')
+  UNION ALL
+  select segment_name, '30d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
+  PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
+  from cycle_time_per_block
+  where segment_finish_time > timestamp_trunc(current_timestamp() - interval 30 day, day, 'America/Los_Angeles')
+  UNION ALL
+  select segment_name, '7d' as time_interval, -- count(child_plate) as child_count_with_data, avg(cycle_hour) as cycle_hour 
+  PERCENTILE_CONT(cycle_hour, 0.5) over (partition by segment_name) as cycle_hour_median,
+  from cycle_time_per_block
+  where segment_finish_time > timestamp_trunc(current_timestamp() - interval 7 day, day, 'America/Los_Angeles')) t
+  on seg_p.segment_name = t.segment_name
+  and seg_p.period = t.time_interval
+  order by segment_rank, period_rank;
