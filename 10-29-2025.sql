b25cf7559073fd25ad645fe0619ace1282d87e24
wenfangdi <158525259+wenfangdi@users.noreply.github.com>
2025-10-29 17:50:54 -0700

    Update 10-29-2025.sql

--- 10-29-2025.sql
+++ 10-29-2025.sql
@@ -1,13 +1,21 @@
 --missing flags for plasma event find
 
+
 select --string_agg(cast(r.id_run_growth as string))
-id_run_growth, end_time,char_block_count_240
+id_run_growth, end_time,char_block_count_240, flag.excursion_types, run_duration, 
+(UNIX_SECONDS(ge.timestamp)-UNIX_SECONDS(r.start_time))/3600 as hour_since_start
 
 from `df-data-and-ai.trilliback.bq_growth_runs_about_block`
 join `df-data-and-ai.trilliback.bq_growth_runs` r using(id_run_growth)
-left join `df-max.raw_dfdb.run_excursion_flagging` flag on 
-where id_run_growth in (27328, 27143, 27798, 27745, 28382, 28233, 28338, 28561, 28707, 28838, 28946, 28504, 28592, 28927,27242,28686,27931,28183,27688,27933,27756,27049,27866,27863,28014,28579,28457,28181,27493,28547,27402,27877,28756,27645,27915,27704,27591,27112,27936,28255,28393,27674,27277)
+left JOIN `df-max.raw_dfdb.run_excursion_flagging` flag using(id_run_growth)
+LEFT JOIN `df-data-and-ai.df_warehouse.growth_events` ge using(id_run_growth)
+where id_run_growth in (28973, 28952, 28937, 28909, 28858, 28842, 28803, 28732, 28703, 28695, 28680, 28666, 28628, 28579, 28547, 28323, 28255, 28183, 28014, 27936, 27877, 27863, 27756, 27704, 27688, 27645, 27493, 27402, 27242, 27112)
 and r.end_time > current_timestamp() - interval 90 day
-and char_block_count_240 > 0
-and r.loading_purpose = 'Production'
 
+and r.loading_purpose = 'Production'
+and excursion_types = 'No Excursions'
+and id_run_growth not in (28927, 28592,28504, 28579 ) -- checked and fixed
+and char_block_count_240 > 0 
+-- and r.run_duration < 680
+-- and id_run_growth = 28686
+order by end_time desc, hour_since_start desc
